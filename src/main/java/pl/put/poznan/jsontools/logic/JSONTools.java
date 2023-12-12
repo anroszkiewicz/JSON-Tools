baaf3ecd912d1e33097e34d022fd668e8bf358ad
anroszkiewicz <anroszkiewicz@gmail.com>
2023-12-12 14:04:46 +0100

    Konfiguracja kontrolera REST API

--- src/main/java/pl/put/poznan/jsontools/logic/JSONTools.java
+++ src/main/java/pl/put/poznan/jsontools/logic/JSONTools.java
@@ -1,18 +1,21 @@
 package pl.put.poznan.jsontools.logic;
 
-/**
- * This is just an example to show that the logic should be outside the REST service.
- */
 public class JSONTools {
 
+    private String jsonString;
     private final String[] transforms;
+    private final String[] filterParams;
+    private final String[] excludeParams;
 
-    public JSONTools(String[] transforms){
+    public JSONTools(String jsonString, String[] transforms, String[] filterParams, String[] excludeParams){
+        this.jsonString = jsonString;
         this.transforms = transforms;
+        this.filterParams = filterParams;
+        this.excludeParams = excludeParams;
     }
 
-    public String transform(String text){
+    public String transform(){
         // of course, normally it would do something based on the transforms
-        return text.toUpperCase();
+        return jsonString.toUpperCase();
     }
 }
