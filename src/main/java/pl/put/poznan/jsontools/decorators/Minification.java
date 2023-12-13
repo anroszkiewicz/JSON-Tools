b66f091c389e44cf2aafa7c15430e010a304449c
kubglo02@gmail.com <kubglo02@gmail.com>
2023-12-13 17:34:23 +0100

    implementacja minifikacji

--- src/main/java/pl/put/poznan/jsontools/decorators/Minification.java
+++ src/main/java/pl/put/poznan/jsontools/decorators/Minification.java
@@ -1,14 +1,39 @@
 package pl.put.poznan.jsontools.decorators;
 
 public class Minification extends JSONDecorator {
-	
+
 	public Minification(JSONString toBeDecorated) {
 		super(toBeDecorated);
 	}
 
 	@Override
 	public String getData() {
-		// implement minification here
-		return super.getData();
+
+		String data = super.getData();
+		String mini = new String();
+		boolean inQuotes = false;
+		boolean escapeMode = false;
+
+		for (int i = 0; i < data.length(); i++) {
+			char sign = data.charAt(i);
+			if (escapeMode == true) {
+				mini = mini.concat(Character.toString(sign));
+				escapeMode = false;
+			} 
+			else if (sign == '"') {
+				inQuotes = !inQuotes;
+				mini = mini.concat(Character.toString(sign));
+			} 
+			else if (sign == '\\') {
+				escapeMode = true;
+				mini = mini.concat(Character.toString(sign));
+			} 
+			else if (inQuotes == false && (sign == ' ' || sign == '\n'))
+				continue;
+			else
+				mini = mini.concat(Character.toString(sign));
+		}
+
+		return mini;
 	}
 }
