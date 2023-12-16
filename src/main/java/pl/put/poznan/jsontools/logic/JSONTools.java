7818e73439fd18deff73df8c8de18f294ee0cf09
kubglo02@gmail.com <kubglo02@gmail.com>
2023-12-16 20:52:12 +0100

    #2 #3 #4 #5 Zgłaszanie błędu dla niepoprawnego formatu JSON

--- src/main/java/pl/put/poznan/jsontools/logic/JSONTools.java
+++ src/main/java/pl/put/poznan/jsontools/logic/JSONTools.java
@@ -1,8 +1,18 @@
 package pl.put.poznan.jsontools.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import pl.put.poznan.jsontools.decorators.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JSONTools {
+
+    private static final Logger logger = LoggerFactory.getLogger(JSONTools.class);
 
     private String jsonString;
     private final String[] transforms;
@@ -17,27 +27,38 @@ public class JSONTools {
     }
 
     public String transform() {
+        // test wheather text is in valid json format
+		ObjectMapper objectMapper = new ObjectMapper();
+        try {
+			JsonNode jsonNode = objectMapper.readTree(jsonString);
+        } 
+        catch (JsonMappingException e) {
+            logger.debug("ERROR: Object mapper failed");
+			return "ERROR: Server side error";
+        } 
+        catch (JsonProcessingException e) {
+            logger.debug("ERROR: Wrong format of input data");
+			return "ERROR: Wrong format of input data";
+        }
 
-		// test wheather text is in valid json format
+        JSONString decorator = new SimpleJSONString(jsonString);
 
-		JSONString decorator = new SimpleJSONString(jsonString);
-
-		for (String transform: transforms) {
-			switch(transform) {
-				case "minify":
-					decorator = new Minification(decorator);
-					break;
-				case "decompress":
-					decorator = new Decompression(decorator);
-					break;
-				case "filter":
-					decorator = new Filter(decorator, filterParams);
-					break;
-				case "exclude":
-					decorator = new Exclude(decorator, excludeParams);
-					break;
-			}
-		}
+        for (String transform: transforms) {
+            switch(transform) {
+                case "minify":
+                    decorator = new Minification(decorator);
+                    break;
+                case "decompress":
+                    decorator = new Decompression(decorator);
+                    break;
+                case "filter":
+                    decorator = new Filter(decorator, filterParams);
+                    break;
+                case "exclude":
+                    decorator = new Exclude(decorator, excludeParams);
+                    break;
+            }
+        }
         return decorator.getData();
     }
 }
