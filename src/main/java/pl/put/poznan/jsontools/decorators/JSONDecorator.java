1461cf82379c97416e2b6eefd18ac972f1516ed8
anroszkiewicz <anroszkiewicz@gmail.com>
2023-12-20 22:11:51 +0100

    Edycja komentarzy

--- src/main/java/pl/put/poznan/jsontools/decorators/JSONDecorator.java
+++ src/main/java/pl/put/poznan/jsontools/decorators/JSONDecorator.java
@@ -1,13 +1,22 @@
 package pl.put.poznan.jsontools.decorators;
-
+/**
+ * A general decorator class that all decorators extend
+ */
 public class JSONDecorator implements JSONString {
 	
 	private JSONString jsonString;
 
+	/** 
+	* Class constructor
+	* @param toBeDecorated input JSON
+	*/
 	public JSONDecorator(JSONString toBeDecorated) {
 		this.jsonString = toBeDecorated;
 	}
 
+	/** 
+	* @return JSON in String form after transformations
+	*/
 	@Override
 	public String getData() {
 		return jsonString.getData();
