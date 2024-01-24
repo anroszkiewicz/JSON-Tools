f461482e4db38c8048701e53532b6c3e1dd97629
anroszkiewicz <111358119+anroszkiewicz@users.noreply.github.com>
2024-01-24 23:02:43 +0100

    #21 Rozszerzenie dokumentacji javadoc

--- src/main/java/pl/put/poznan/jsontools/decorators/Minification.java
+++ src/main/java/pl/put/poznan/jsontools/decorators/Minification.java
@@ -1,11 +1,24 @@
 package pl.put.poznan.jsontools.decorators;
-
+/**
+ * Class that performs minification on a given JSON. 
+ * Unnecessary characters (spaces and new lines) are removed.
+ */
 public class Minification extends JSONDecorator {
 
+	/** 
+	* Class constructor
+	* @param toBeDecorated input JSONString
+	*/
 	public Minification(JSONString toBeDecorated) {
 		super(toBeDecorated);
 	}
 
+	/**
+	 * Method that performs minification.
+	 * Parses the given JSON character by character.
+	 * Removes all spaces and new line characters.
+	 * @return String containing JSON in minified form
+	*/
 	@Override
 	public String getData() {
 
